f069b7ba00e16904f0dcb3e801a98c85bce366cd
MegaMidnight <richarr916@gmail.com>
2022-03-29 10:41:52 +0000

    Update at 2022-03-29T10:41:52.914393Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,3 +1,7 @@
 package com.bytelegend;
 
-public class Challenge {}
+public class Challenge {
+    public static void main(String[] args) {
+        System.out.println("returning void (unit w/e) here")
+    }
+}
