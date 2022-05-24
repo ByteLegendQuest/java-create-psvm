0b5fc1912dddb2813f952ec92d08123c4c93534a
chinahzh <2987140244@qq.com>
2022-05-24 07:39:26 +0000

    Update at 2022-05-24T07:39:26.175811Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,3 +1,10 @@
 package com.bytelegend;
 
-public class Challenge {}
+public class Challenge {
+    public static void main(String[]args){
+        System.out.println("hello,world.")
+    }
+}
+
+
+main()
