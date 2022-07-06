4a0c7e9242fa9141aa7fd2ef45e84295f88f5352
DescentDD <1740811552@qq.com>
2022-07-06 14:00:23 +0000

    Update at 2022-07-06T14:00:23.488528Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,8 @@
 package com.bytelegend;
 
-public class Challenge {}
+public class Challenge {
     public static void main(String[] args) {
              System.out.println("Hello world");
         
     }
+}
