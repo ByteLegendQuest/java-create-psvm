734cf283edc7236b3f3dfced245acbc1cb7559b5
wang54541 <wang54541@126.com>
2022-07-12 15:42:58 +0000

    Update at 2022-07-12T15:42:58.008906Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,3 +1,22 @@
 package com.bytelegend;
 
-public class Challenge {}
+import javax.print.DocFlavor;
+
+public class Challenge {
+    public static void main(String[] args) {
+        System.out.println(hello());
+        System.out.println(add(40, 2));
+    }
+
+    public static String hello() {
+        String msg;
+        msg="hello";
+        return msg;
+    }
+
+    public static int add(int a,int b) {
+        int result;
+        result=a+b;
+        return  result;
+    }
+}
