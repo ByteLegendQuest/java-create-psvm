2203eabd20f547c5f9c06c573d484cc201d68365
ljsalt <lj27128176@outlook.com>
2022-12-06 03:27:54 +0000

    Update at 2022-12-06T03:27:54.417840Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
 
-public class Challenge {
-    public static void main(string[] args){
-        
+public class Challenge  {
+    public static void main(string[] args) {
+
     }
 }
